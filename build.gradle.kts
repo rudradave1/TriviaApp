b08c23881a38fd9bafd52d8b5123bedb7ed1165a
MJ Jacobs <marthinusjacobs@gmail.com>
2025-01-01 15:27:07 +0200

    Migrated from build.gradle to build.gradle.kts
    Took 5 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
+    
     repositories {
         google()
         mavenCentral()
     }
+    
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.7.3'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0"
+        classpath ("com.android.tools.build:gradle:8.7.3")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -17,10 +19,5 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
     }
-}
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
 }
